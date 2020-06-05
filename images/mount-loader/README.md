f2bfa006d879061b38bd8d158a74b41cb67dc119
Alan Christie <alan.christie@matildapeak.com>
2020-06-05 16:03:13 +0100

    - Updated loader

--- images/mount-loader/README.md
+++ images/mount-loader/README.md
@@ -1,12 +1,11 @@
-# Fragalysis S3 Data Loader Image
+# Fragalysis Mounted Volume Data Loader Image
 An extension of the `xchem/fragalysis-loader` image that first
-copies files form an S3 bucket before calling the loader's
-`run_loader.sh` script.
+copies files from a volume mounted at `/mounted-media` before calling
+the loader's `run_loader.sh` script.
 
 ## Building
 Build and push with: -
 
-    $ docker build . -t informaticsmatters/fragalysis-s3-loader:latest \
-        --build-arg from_image=informaticsmatters/fragalysis-loader \
-        --build-arg from_tag=im-travis
-    $ docker push informaticsmatters/fragalysis-s3-loader:latest
+    $ docker build . -t informaticsmatters/fragalysis-mounted-loader:latest \
+        --build-arg from_image=informaticsmatters/fragalysis-loader
+    $ docker push informaticsmatters/fragalysis-mounted-loader:latest
--- images/mount-loader/docker-entrypoint.sh
+++ images/mount-loader/docker-entrypoint.sh
@@ -3,22 +3,25 @@
 # Does the container look intact?
 # We need numerous environment variables.
 
-: "${AWS_ACCESS_KEY_ID?Need to set AWS_ACCESS_KEY_ID}"
-: "${AWS_SECRET_ACCESS_KEY?Need to set AWS_SECRET_ACCESS_KEY}"
-: "${BUCKET_NAME?Need to set BUCKET_NAME}"
 : "${DATA_ORIGIN?Need to set DATA_ORIGIN}"
+
+SRC=/mounted-media
 
 if [ ! -d "/code/media" ]; then
     echo "ERROR - the destination directory (/code/media) does not exist"
     exit 0
 fi
+if [ ! -d "$SRC" ]; then
+    echo "ERROR - the source directory ($SRC) does not exist"
+    exit 0
+fi
 
 # OK if we get here...
 #
-# We expect data to be available on S3 in the BUCKET_NAME provided in the
-# 'django-data' sub-directory using the path specified in DATA_ORIGIN.
-# The bucket data is copied to the mount point '/code/media',
-# i.e. data in s3://${BUCKET_NAME)/django-data/${DATAT_ORIGIN}
+# We expect data to be available in /mounted-media
+# using the sub-path specified in DATA_ORIGIN.
+# We copy data to the mount point '/code/media',
+# i.e. data in '/mounted-media/${DATA_ORIGIN}'
 # is written to '/code/media/NEW_DATA'.
 #
 # - Wipe the (temporary) destination directory
@@ -28,8 +31,8 @@ DST=/code/media/NEW_DATA
 echo "+> Removing ${DST}"
 rm -rf ${DST}
 mkdir ${DST}
-echo "+> Syncrhonising ${DATA_ORIGIN} to ${DST}..."
-aws s3 sync "s3://${BUCKET_NAME}/django-data/${DATA_ORIGIN}" /code/media/NEW_DATA
+echo "+> Copying ${SRC}/${DATA_ORIGIN} to ${DST}..."
+cp -R "${SRC}/${DATA_ORIGIN}/" /code/media/NEW_DATA
 echo "+> Running loader..."
 ./run_loader.sh
 echo "+> Done."
